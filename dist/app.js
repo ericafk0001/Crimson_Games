fab61fdce4294bfba3aaa97fa8bfb07fa38c43f3
Eric Lin <dxslyofficial@gmail.com>
2024-01-02 17:17:55 -0500

    New Commit

--- dist/app.js
+++ dist/app.js
@@ -4,7 +4,9 @@ let games = document.querySelectorAll('.game');
 input.addEventListener('input', function (e) {
   const term = this.value.toLowerCase();
   games.forEach(function (game) {
-    const gameText = game.querySelector('.header').textContent.toLowerCase(); // or .innerText.toLowerCase();
+    const gameText = game
+      .querySelector('.game-title')
+      .textContent.toLowerCase(); // or .innerText.toLowerCase();
     game.hidden = !gameText.includes(term);
   });
 });
